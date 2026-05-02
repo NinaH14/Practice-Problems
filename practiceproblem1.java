fe5c658f1f4eebd9a994e0b7e4b7945d90c20ac4
NinaH41 <ninah41@nycstudents.net>
2026-05-01 20:55:39 -0400

    second commit

--- practiceproblem1.java
+++ practiceproblem1.java
@@ -1,14 +1,14 @@
 public class practiceproblem1{
 
-public static int countOccurrences (int arr[],int n) {
-    int count = 0;
+public static int countOccurrences (int arr[],int n) { //Counts how many times the number n appears in the array
+    int count = 0; // start count at 0
  int m = 0;
-for (m = 0; m < arr.length; m++)
-    if ( arr[m] == n) {
+for (m = 0; m < arr.length; m++) // Go through every number in the list one by one
+    if ( arr[m] == n) { //If the current number matches the one we are looking for, add 1 to our count
      count++; }
      return count;
     }
-public static void main(String[] args) {
+public static void main(String[] args) { // Everything in here happens as soon as you hit "Run"
     int[] arr = {0, 1, 1, 0, 7, 9, 7, 5, 6, 5, 4, 2,  0, 1, 0, 2, 2, 1, 7, 9, 0, 4, 5, 6, 3, 2, 3, 0, 2, 0, 3, 2, 1, 0, 7, 5, 7, 7, 4, 9, 1, 0, 2, 1, 2, 0, 7, 9, 6, 5, 9, 6, 0, 3, 4, 6, 4, 7, 6, 5, } ;
     int result = countOccurrences(arr, 1);
      System.out.println("The number 1 appears: " + result + " times.");
