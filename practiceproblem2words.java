0f8cdc11388ea596697bf98eae7678f3da2966a9
NinaH41 <ninah41@nycstudents.net>
2026-05-01 21:03:16 -0400

    second commit

--- practiceproblem2words.java
+++ practiceproblem2words.java
@@ -4,12 +4,12 @@ public class practiceproblem2words {
     
     int m = 0;
 
-    char[] letters = input.toCharArray();
-     char[] reversed = new char[letters.length];
+    char[] letters = input.toCharArray(); // turn words into an array of individual letters and characters
+     char[] reversed = new char[letters.length]; // create an new array that can hold the reverse order of the original array
 
      
-     for (m = 0; m <  letters.length; m++) { 
-    reversed[m] = letters[letters.length - 1 - m];
+     for (m = 0; m <  letters.length; m++) { // go through array of the letters on by one, first to last
+    reversed[m] = letters[letters.length - 1 - m]; // putting the last letter of the original array and putting it in the first spot of the new reverse array
 }
     return reversed ; 
     }
